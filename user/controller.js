8060f45d429dc46f121ddeaa437b376f2fa93488
viktr <viktrantonyan1998@gmail.com>
2021-11-16 10:18:30 +0400

    Finishing hw

--- user/controller.js
+++ user/controller.js
@@ -1,62 +1,131 @@
 const { connection } = require('../database/connection');
 const { StatusCodes } = require('http-status-codes');
 const { validationResult } = require('express-validator');
+const {getUserByEmail, getUserById} = require("../utils/queryHp");
 
 class User {
 
     // Get all
-    static index = (req, res) => {
-
+    static index = async (req, res, next) => {
+        try {
+            await connection.query('SELECT * FROM members', function (error, results, fields) {
+                if (error) throw error;
+                res.status(StatusCodes.OK)
+                res.json({
+                    status: 'ok',
+                    members: results
+                })
+            });
+        } catch (e) {
+            next(e)
+        }
     }
 
     // Get by :id
-    static show = async (req, res) => {
-        let userData = new Promise((resolve, reject) => {
-            connection.query('select * from `users` where id=?', [req.params.id], function (error, results, fields) {
-                if (error) return reject(error);
-                console.log("Line 2")
-                console.table(results);
-
-                resolve(results)
+    static show = async (req, res, next) => {
+        try {
+            const {id} = req.params
+            await connection.query(`SELECT * FROM members WHERE id = ${id}`, function (error, results, fields) {
+                if (error) throw error;
+                res.status(StatusCodes.OK)
+                res.json({
+                    data: results
+                })
             });
-        });
-
-        console.log("Line 4")
-        let result = await userData;
-        console.log(result)
-        console.log("Line 5")
-
-        console.log("Line 3")
-
-        return res.status(StatusCodes.OK).json({ message: 'success', data: result });
+        } catch (e) {
+            next(e)
+        }
     }
 
     // Create a new user
-    static create = function (req, res) {
-        const errors = validationResult(req);
+    static create = async function (req, res, next) {
+        try {
+            let now = new Date();
+            const momentDate = moment(now).format('YYYY-MM-DD')
 
-        connection.query('INSERT INTO `users` (`name`,`gender`,`dob`) VALUES (?,?,?)', [req.body.name, req.body.gender, req.body.dob], function (error, results, fields) {
-            if (error) throw error;
-
-            console.table(results)
-        })
-
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return next(HttpError('validation error', errors.array()))
+            }
+            const {first_name, last_name, email, date_of_birth} = req.body
+            const created_at = momentDate
+            const updated_at = momentDate
+            const candidate = await getUserByEmail(email)
+            if (candidate[0]) {
+                res.json({
+                    status: 'error',
+                    message: "email already exists"
+                })
+            }
+            await connection.query(`INSERT INTO members(first_name, last_name, email, date_of_birth, created_at, updated_at) 
+                                    VALUES('${first_name}','${last_name}','${email}','${date_of_birth}','${created_at}','${updated_at}')`,
+                function (error, results, fields) {
+                    if (error) throw error;
+                    res.status(StatusCodes.CREATED)
+                    res.json({
+                        message: 'success'
+                    })
+                });
+        } catch (e) {
+            next(e)
         }
 
         return res.status(201).json({ message: 'success' });
     }
 
     // Update user by :id
-    static update = function (req, res) {
-        // Create a new user
-    }
+    static update = async function (req, res, next) {
+        try {
+            let now = new Date();
+            const momentDate = moment(now).format('YYYY-MM-DD')
+            const errors = validationResult(req)
+            const updated_at = momentDate
+
+            if (!errors.isEmpty()) {
+                return next(HttpError('validation error', errors.array()))
+            }
+            const {first_name, last_name, email, date_of_birth} = req.body
+            const candidate = await getUserByEmail(email);
+            if (!candidate[0]) {
+                res.json({
+                    status: 'error',
+                    message: "not a user in email" + email
+                })
+            }
+
+            await connection.query(`UPDATE members SET first_name = '${first_name}', last_name = '${last_name}', date_of_birth = '${date_of_birth}', updated_at = '${updated_at}'  
+                WHERE email = '${email}'`, function (error, results, fields) {
+                if (error) throw error;
+
+                res.status(StatusCodes.UPDATE)
+                res.json({
+                    message: 'user successfully updated'
+                })
+            });
+        } catch (e) {
+            next(e)
+        }    }
 
     // Delete user by :id
-    static destroy = function (req, res) {
-        // Create a new user
-    }
+    static destroy = async function (req, res, next) {
+        try {
+            let {id} = req.params
+            const candidate = await getUserById(id);
+            if (!candidate[0]) {
+                throw new Error('not user in id' + id)
+            }
+
+            await connection.query(`DELETE FROM members WHERE id = '${id}'`, function (error, results, fields) {
+                if (error) throw error;
+
+                res.status(StatusCodes.DELETE)
+                res.json({
+                    message: 'user successfully deleted'
+                })
+            });
+        } catch (e) {
+            next(e)
+        }    }
 }
 
 module.exports = { User }
